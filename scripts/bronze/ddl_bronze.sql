dc83d01374d55032cbe88728460eb6bd3e59a389
Darko370 <milicdarko2010@gmail.com>
2025-09-05 19:46:30 +0200

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -7,7 +7,7 @@ Script Purpose:
     alredy exist in database.
     Run this script to re-define the DDL structure of bronze tables
 =======================================================================================
-/*
+*/
 
 IF OBJECT_ID( 'bronze.crm_cust_info', 'U') IS NOT NULL
 DROP TABLE bronze.crm_cust_info;
